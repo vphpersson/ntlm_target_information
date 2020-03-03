bf7f300b43e55e271744d01c049168a09b3a1a82
Viktor Persson <vph.persson@gmail.com>
2020-03-03 11:16:38 +0100

    Minor refactoring.

--- ntlm_target_information.py
+++ ntlm_target_information.py
@@ -4,6 +4,7 @@ from re import compile as re_compile
 from argparse import ArgumentParser
 from enum import Enum, auto
 from asyncio import run as asyncio_run
+from sys import stderr
 
 from aiohttp import ClientSession, ClientTimeout
 from ntlm.structures.av_pair import EOLAVPair
@@ -15,8 +16,8 @@ NTLM_PATTERN = re_compile('^NTLM (?P<ntlm_data>[^,]+).*$')
 
 
 class TargetInformationMode(Enum):
-    HTTP_OWA = auto()
-    LDAP_AD = auto()
+    HTTP = auto()
+    LDAP = auto()
 
 
 def get_parser() -> ArgumentParser:
@@ -27,9 +28,9 @@ def get_parser() -> ArgumentParser:
     subparsers = parser.add_subparsers(help='The method with which to retrieve the target information.')
 
     http_owa_parser = subparsers.add_parser(
-        name=TargetInformationMode.HTTP_OWA.name.lower()
+        name=TargetInformationMode.HTTP.name.lower()
     )
-    http_owa_parser.set_defaults(which=TargetInformationMode.HTTP_OWA)
+    http_owa_parser.set_defaults(mode=TargetInformationMode.HTTP)
 
     http_owa_parser.add_argument(
         'url',
@@ -45,35 +46,39 @@ def get_parser() -> ArgumentParser:
     )
 
     ldap_ad_parser = subparsers.add_parser(
-        name=TargetInformationMode.LDAP_AD.name.lower()
+        name=TargetInformationMode.LDAP.name.lower()
         # parents=[LdapServerAddressParser()]
     )
-    ldap_ad_parser.set_defaults(which=TargetInformationMode.LDAP_AD)
+    ldap_ad_parser.set_defaults(mode=TargetInformationMode.LDAP)
 
     return parser
 
 
-async def main():
-    args = get_parser().parse_args()
+async def ntlm_target_information():
+
+    parser = get_parser()
+    args = parser.parse_args()
+
+    if getattr(args, 'mode', None) is None:
+        return parser.error(message='No mode selected.')
 
     async with ClientSession(timeout=ClientTimeout(total=args.timeout)) as client_session:
         av_pairs = (await retrieve_http_ntlm_challenge(client_session=client_session, url=args.url)).target_info
 
     if av_pairs is None:
-        # TODO: Use proper exception.
-        raise ValueError('no av pairs')
-
-    print(
-        '\n'.join(sorted([
-            f'{av_pair.LABEL}: {av_pair.get_value()}'
-            for av_pair in av_pairs
-            if not isinstance(av_pair, EOLAVPair)
-        ]))
-    )
+        print('No NTLM target information available.', file=stderr)
+    else:
+        print(
+            '\n'.join(sorted([
+                f'{av_pair.LABEL}: {av_pair.get_value()}'
+                for av_pair in av_pairs
+                if not isinstance(av_pair, EOLAVPair)
+            ]))
+        )
 
 
 if __name__ == '__main__':
-    asyncio_run(main())
+    asyncio_run(ntlm_target_information())
 
 
 
