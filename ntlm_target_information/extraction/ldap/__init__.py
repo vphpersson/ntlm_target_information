d721ce4df29738f57220c7b367d96e7c2af1104e
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2020-03-03 15:53:24 +0100

    Major refactor.

---
+++ ntlm_target_information/extraction/ldap/__init__.py
@@ -0,0 +1,34 @@
+from contextlib import contextmanager
+
+from ldap3 import Connection as Ldap3Connection
+from ldap3.utils.ntlm import NtlmClient
+
+from ntlm.messages.challenge import ChallengeMessage
+
+
+def retrieve_ad_ldap_ntlm_challenge(connection: Ldap3Connection) -> ChallengeMessage:
+    """
+    Extract the NTLM challenge message from NTLM authentication procedure over LDAP.
+
+    :param connection: An unbound LDAP connection.
+    :return: An NTLM challenge message resulting from an attempt to bind.
+    """
+    # Intercept the parsing of the LDAP server’s NTLM `CHALLENGE_MESSAGE` and extract the domain name from the
+    # `DnsDomainName` value (FQDN of the domain) of the `TargetInfo` field.
+
+    challenge_message = None
+
+    def parse_challenge_message_wrapper(self, data: bytes) -> None:
+        nonlocal challenge_message
+        challenge_message = ChallengeMessage.from_bytes(data=data)
+
+    @contextmanager
+    def substitute_parse_challenge_message():
+        parse_challenge_message_backup = NtlmClient.parse_challenge_message
+        NtlmClient.parse_challenge_message = parse_challenge_message_wrapper
+        yield
+        NtlmClient.parse_challenge_message = parse_challenge_message_backup
+
+    with substitute_parse_challenge_message():
+        connection.bind()
+        return challenge_message
