8656aed9b4d8b973ef744e80170384a9c2d70f71
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2020-12-27 00:23:28 +0100

    Restructure and clean up.
    - Fix reference problems.
    - Use `httpx` instead of `aiohttp`.
    - Use a log handler for error messages.
    - Use a `ArgumentParser` class.
    
    NOTE: `pipenv` is having problems retrieving the `ntlm` dependency...
    Not sure why. Deal breaker....

--- ntlm_target_information/extraction/http/errors.py
+++ ntlm_target_information/extraction/http/errors.py
@@ -1,19 +1,19 @@
-from aiohttp import ClientResponse
+from httpx import Response
 
-from ntlm_target_information.exceptions import ExtractionError
+from ntlm_target_information.errors import ExtractionError
 
 
 class HTTPNTLMExtractionError(ExtractionError):
-    def __init__(self, msg: str, response: ClientResponse):
+    def __init__(self, msg: str, response: Response):
         super().__init__(msg)
-        self.response: ClientResponse = response
+        self.response: Response = response
 
 
 class WWWAuthenticateNotInHeadersError(HTTPNTLMExtractionError):
-    def __init__(self, response: ClientResponse):
+    def __init__(self, response: Response):
         super().__init__(msg='No WWW-Authenticate header in response.', response=response)
 
 
 class NoNTLMAuthenticationTypeError(HTTPNTLMExtractionError):
-    def __init__(self, response: ClientResponse):
+    def __init__(self, response: Response):
         super().__init__(msg='No NTLM authentication type in WWW-Authenticate header.', response=response)
