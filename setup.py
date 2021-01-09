0f1454b15d1a1e7eef2d58298b15ef06273f0de4
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2021-01-09 22:20:44 +0100

    Fix `setup.py`.

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'ntlm @ https://github.com/vphpersson/ntlm/tarball/master',
-        'aiohttp',
+        'ntlm @ git+ssh://git@github.com/vphpersson/ntlm.git#egg=ntlm',
+        'pyutils @ git+ssh://git@github.com/vphpersson/pyutils.git#egg=pyutils',
+        'terminal_utils @ git+ssh://git@github.com/vphpersson/terminal_utils.git#egg=terminal_utils',
+        'httpx',
         'ldap3'
     ]
 )
